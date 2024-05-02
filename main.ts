0680fbb9a805af49ff59f0e739cfefc0bf33374f
Shafaq Mughal <160257641+shafaqmughal20@users.noreply.github.com>
2024-05-02 08:12:06 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+//32.Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+//• Make a list of five or more usernames called current_users.
+
+//• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+//• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+//• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+
+//current users array
+let current_users =["Aslam","usama","taboo","sadaf","hamza"] 
+
+//current users array
+let new_users =["usama","laiba","areeba","mani","hamza"]
+
+//loop through new_users to chech for usernames availability
+ new_users.forEach(new_one_user => {
+
+    //making a condition for username already exist and save in our_condition variable
+    let our_condition= current_users.some(current_one_user =>current_one_user.toLowerCase()===new_one_user.toLocaleLowerCase())
+
+    //print different message using if else statement
+    if(our_condition)
+      {console.log(`Sorry ${new_one_user} is already taken!`)}
+    else 
+    {console.log(`This username ${new_one_user} is available`)}
+ })
